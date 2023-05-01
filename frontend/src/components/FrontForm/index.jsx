922dca9f085ade44cd321176c045d59f4e0f0712
S-ishita <singhalishita016@gmail.com>
2023-05-01 17:59:09 +0530

    frontend enhanced

--- frontend/src/components/FrontForm/index.jsx
+++ frontend/src/components/FrontForm/index.jsx
@@ -4,9 +4,16 @@ import "./style.css";
 
 export default function FrontForm() {
   return (
-    <div className="frontForm">
-      <h1>OpenDevX</h1>
-      <InputField/>
+    <div class="formbody">
+      <img src="../osslogo.png" className="logo" alt="logo" />
+      <div className="frontForm">
+        <h1>OpenDevX</h1>
+        <InputField />
+      </div>
+      <div class="leftside">
+        <img src="../earth.png" className="earth" alt="earth" />
+        <img src="../peoples.png" className="people" alt="people" />
+      </div>
     </div>
   );
 }
