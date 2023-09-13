20d27f0fb8a5937b3ae5f3e76e6f3570aaa69bd1
CulturalProfessor <svinayak994@gmail.com>
2023-09-13 21:41:28 +0530

    fix : updated rate-limiter

--- server/middlewares/rate-limiter.js
+++ server/middlewares/rate-limiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 // Set up rate limiter middleware
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 10, // limit each IP to 5 requests per minute
+  max: 3, // limit each IP to 3 requests per minute
   message: "Too many requests from this IP, please try again after a minute",
 });
 
