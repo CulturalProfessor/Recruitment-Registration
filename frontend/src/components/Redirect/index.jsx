44fee64e7248abdc03a0f683bbb62288cd20bb5c
CulturalProfessor <svinayak994@gmail.com>
2023-09-19 23:05:29 +0530

    feat : closing registration

--- frontend/src/components/Redirect/index.jsx
+++ frontend/src/components/Redirect/index.jsx
@@ -8,8 +8,8 @@ export default function Redirect() {
         <img src="/osslogo.png" className="redirectLogo" alt="logo" />
         <h1 className="formHeadingElement">Team OSS </h1>
       </div>
-      <h1>You have successfully registered</h1>
-      <h1>Welcome Aboard</h1>
+      <h1>Registrations have closed</h1>
+      <h1>Thanks for your great response</h1>
       <img src="/success.svg" id="redirectImage" />
     </div>
   );
