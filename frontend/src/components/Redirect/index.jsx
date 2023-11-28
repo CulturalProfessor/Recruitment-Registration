0116bc216f1c11a73d5632639737c2a6380d212a
CulturalProfessor <svinayak994@gmail.com>
2023-11-29 00:14:51 +0530

    fix : Redirect component

--- frontend/src/components/Redirect/index.jsx
+++ frontend/src/components/Redirect/index.jsx
@@ -8,8 +8,8 @@ export default function Redirect() {
         <img src="/osslogo.png" className="redirectLogo" alt="logo" />
         <h1 className="formHeadingElement">Team OSS </h1>
       </div>
-      <h1>Registrations have closed</h1>
-      <h1>Thanks for your great response</h1>
+      <h1>Thanks for Registration</h1>
+      <h1>Confirmation mail will be sent.</h1>
       <img src="/success.svg" id="redirectImage" />
     </div>
   );
